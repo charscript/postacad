f277003d06de13839a724a3b21c40a18cef07c5d
charscript <marcos.martinez@estudiante.ceibal.edu.uy>
2024-08-01 02:01:24 -0300

    nueva feature: fullscreen en small devices on click

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,17 +1,42 @@
-import Bottombar from '@/components/shared/Bottombar'
-import LeftSidebar from '@/components/shared/LeftSidebar'
-import Topbar from '@/components/shared/Topbar'
-import { Outlet } from 'react-router-dom'
-import RightSidebar from '@/components/shared/RightSidebar'
-import { useState } from 'react'
+import Bottombar from '@/components/shared/Bottombar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import Topbar from '@/components/shared/Topbar';
+import { Outlet } from 'react-router-dom';
+import RightSidebar from '@/components/shared/RightSidebar';
+import { useState } from 'react';
 
 const RootLayout = () => {
-
   const [isOpen, setIsOpen] = useState(false);
+
+  function getFullscreenElement() {
+    return document.fullscreenElement;
+  }
+
+  function toggleFullscreen() {
+    if (getFullscreenElement()) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen().catch((e) => {
+        console.log(e);
+      });
+    }
+  }
+
+  function isSmallDevice() {
+    return window.matchMedia("(max-width: 768px)").matches; // Ajusta el tamaño según tus necesidades
+  }
 
   return (
     <>
-      <div className="w-full md:flex">
+      <div
+        className="w-full md:flex"
+        onClick={() => {
+          if (isSmallDevice()) {
+            toggleFullscreen();
+          }
+        }}
+        style={{ cursor: 'pointer' }}
+      >
         <Topbar />
         <LeftSidebar />
 
@@ -22,7 +47,7 @@ const RootLayout = () => {
         <Bottombar />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
